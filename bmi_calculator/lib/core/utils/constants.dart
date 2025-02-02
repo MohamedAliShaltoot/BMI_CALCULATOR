8cf3edbd4c1e3e31b30af02ebb6757b799704bf1
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-02-03 01:55:35 +0200

    make constants class

---
+++ bmi_calculator/lib/core/utils/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const String applicationScreenTitle = "BMI Calculator";
+  static const String resultScreenTitle = "BMI RESULTS";
+  static const String male = "MALE";
+  static const String female = "FEMALE";
+  static const String height = "HEIGHT";
+  static const String weight = "WEIGHT";
+  static const String age = "AGE";
+  static const String calculate = "Check Your BMI";
+  static const String result = "RESULT";
+  static const String maleImage = "images/male-gender.png";
+  static const String femaleImage = "images/femenine.png";
+  static const Color backgroundColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color appBarBackgroundColor = Colors.blue;
+}
