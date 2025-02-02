7bae6ef0e1fe3598df1065874aaf1c9a00bec4ff
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-02-03 01:56:28 +0200

    Refactoring code

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:bmi_calculator/check_result.dart';
+import 'package:bmi_calculator/Screens/check_result.dart';
+
 
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,9 @@ class BMI extends StatefulWidget {
 class _BMIState extends State<BMI> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: Check());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Check(),
+    );
   }
 }
